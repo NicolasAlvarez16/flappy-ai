0baa3909ef8721d5f4ba5db844ed62c53083eff4
Nicolas <nico@MacBook-Air-de-Admin.local>
2021-03-14 19:13:40 +0000

    Base and Pipes of the game

--- flappy-bird.py
+++ flappy-bird.py
@@ -84,15 +84,90 @@ class Bird:
         """Collision"""
         return pygame.mask.from_surface(self.img)
 
+class Pipe:
+    GAP = 200
+    VEL = 5
 
-def draw_window(window, bird):
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+    
+    def move(self):
+        self.x -= self.VEL
+    
+    def draw(self, window):
+        window.blit(self.PIPE_TOP, (self.x, self.top))
+        window.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+    
+    def collide(self, bird):
+        """Returns wether the bird collided with the pipe or not ---> Using mask for an accurate collision"""
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        # How far away the two top-left hand corners are from each other
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        bottom_point = bird.mask.overlap(bottom_mask, bottom_offset) # Collision point for the bottom pipe (Return none)
+        top_point = bird.mask.overlap(top_mask, top_offset) # Collision point for the top pipe (Retur none)
+
+        if top_point or bottom_point:
+            return True
+        return False
+
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        # Two ground images created
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            # Check if images is off the screen completely
+            self.x1 = self.x2 + self.WIDTH # Move it back again
+        
+        if self.x2 + self.WIDTH < 0:
+            # Check if images is off the screen completely
+            self.x2 = self.x1 + self.WIDTH # Move it back again
+
+    def draw(self, window):
+        window.blit(self.IMG, (self.x1, self.y))
+        window.blit(self.IMG, (self.x2, self.y))
+
+def draw_window(window, bird, pipes, base):
     window.blit(BG_IMG, (0, 0))
+    for pipe in pipes:
+        pipe.draw(window)
+    
+    base.draw(window)
     bird.draw(window)
     pygame.display.update()
 
 
 def main():
-    my_bird = Bird(200, 200)
+    my_bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     window = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     run = True
@@ -101,8 +176,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        my_bird.move()
-        draw_window(window, my_bird)
+        # my_bird.move()
+        for pipe in pipes:
+            pipe.move()
+            
+        base.move()
+        draw_window(window, my_bird, pipes, base)
     pygame.quit()
     quit()
 
